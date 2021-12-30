8cbfa1e1a150f2b1ca463f5cba821ba6b0e791d0
Dmitry Voloshin <woloshindima@gmail.com>
2021-12-30 15:33:18 -0800

    fix

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -63,7 +63,7 @@ function updateUserList(socketIds) {
   });
 }
 
-const socket = io.connect("localhost:5000");
+const socket = io.connect("ultra-chat.herokuapp.com");
 
 socket.on("update-user-list", ({ users }) => {
   updateUserList(users);
