e0b3d446907a3a57567e477baa92693b3cbbaa62
Dmitry Voloshin <woloshindima@gmail.com>
2021-12-30 14:50:33 -0800

    add production link

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -63,7 +63,7 @@ function updateUserList(socketIds) {
   });
 }
 
-const socket = io.connect("localhost:5000");
+const socket = io.connect("ultra-chat.vercel.app");
 
 socket.on("update-user-list", ({ users }) => {
   updateUserList(users);
