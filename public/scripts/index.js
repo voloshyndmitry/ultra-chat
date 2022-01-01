2b7a6cac445a5c5bddfd12af9ab04a661d33cf7d
Dmitry Voloshin <woloshindima@gmail.com>
2022-01-01 06:04:41 -0800

    logick update

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,6 +2,8 @@ let isAlreadyCalling = false;
 let getCalled = false;
 
 const existingCalls = [];
+const socket = io.connect("ultra-chat.herokuapp.com");
+const talkingWithInfo = document.getElementById("talking-with-info");
 
 const { RTCPeerConnection, RTCSessionDescription } = window;
 
@@ -25,15 +27,18 @@ function createUserItemContainer(socketId) {
   userContainerEl.setAttribute("class", "active-user");
   userContainerEl.setAttribute("id", socketId);
   usernameEl.setAttribute("class", "username");
-  usernameEl.innerHTML = `Socket: ${socketId}`;
+  usernameEl.innerHTML = `User: ${socketId}`;
 
   userContainerEl.appendChild(usernameEl);
 
   userContainerEl.addEventListener("click", () => {
+    if (isAlreadyCalling) {
+      return;
+    }
+    isAlreadyCalling = true;
     unselectUsersFromList();
     userContainerEl.setAttribute("class", "active-user active-user--selected");
-    const talkingWithInfo = document.getElementById("talking-with-info");
-    talkingWithInfo.innerHTML = `Talking with: "Socket: ${socketId}"`;
+    talkingWithInfo.innerHTML = `Calling ...`;
     callUser(socketId);
   });
 
@@ -42,72 +47,59 @@ function createUserItemContainer(socketId) {
 
 async function callUser(socketId) {
   const offer = await peerConnection.createOffer();
-  await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
-
-  socket.emit("call-user", {
-    offer,
-    to: socketId,
-  });
-}
-
-function updateUserList(socketIds) {
-  const activeUserContainer = document.getElementById("active-user-container");
-
-  socketIds.forEach((socketId) => {
-    const alreadyExistingUser = document.getElementById(socketId);
-    if (!alreadyExistingUser) {
-      const userContainerEl = createUserItemContainer(socketId);
-
-      activeUserContainer.appendChild(userContainerEl);
-    }
-  });
-}
-
-const socket = io.connect("ultra-chat.herokuapp.com");
-
-socket.on("update-user-list", ({ users }) => {
-  updateUserList(users);
-});
-
-socket.on("remove-user", ({ socketId }) => {
-  const elToRemove = document.getElementById(socketId);
-
-  if (elToRemove) {
-    elToRemove.remove();
+  try {
+    await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
+    socket.emit("call-user", {
+      offer,
+      to: socketId,
+    });
+  } catch (error) {
+    isAlreadyCalling = false;
   }
-});
+}
 
 socket.on("call-made", async (data) => {
+  console.log("call-made<<<");
   if (getCalled) {
-    const confirmed = confirm(
-      `User "Socket: ${data.socket}" wants to call you. Do accept this call?`
-    );
-    console.log("confirmed >>>>", confirmed);
-
-    if (!confirmed) {
-      socket.emit("reject-call", {
-        from: data.socket,
-      });
-
-      return;
-    }
+    return;
   }
-
-  await peerConnection.setRemoteDescription(
-    new RTCSessionDescription(data.offer)
+  const confirmed = confirm(
+    `User "Socket: ${data.socket}" wants to call you. Do accept this call?`
   );
-  
-  const answer = await peerConnection.createAnswer();
-  await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+  console.log("confirmed >>>>", confirmed);
 
-  socket.emit("make-answer", {
-    answer,
-    to: data.socket,
-  });
-  getCalled = true;
+  if (!confirmed) {
+    socket.emit("reject-call", {
+      from: data.socket,
+    });
+
+    return;
+  }
+  //   }
+
+  talkingWithInfo.innerHTML = `Talking with ${data?.socket}`;
+  try {
+    await peerConnection.setRemoteDescription(
+      new RTCSessionDescription(data.offer)
+    );
+
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+    socket.emit("make-answer", {
+      answer,
+      to: data.socket,
+    });
+    getCalled = true;
+  } catch (error) {
+    getCalled = false;
+    isAlreadyCalling = false;
+    alert(error);
+    console.log("error: ", error);
+  }
 });
 
 socket.on("answer-made", async (data) => {
+  console.log("answer-made<<<");
   await peerConnection.setRemoteDescription(
     new RTCSessionDescription(data.answer)
   );
@@ -119,9 +111,14 @@ socket.on("answer-made", async (data) => {
 });
 
 socket.on("call-rejected", (data) => {
+  console.log("call-rejected<<<");
   alert(`User: "Socket: ${data.socket}" rejected your call.`);
   unselectUsersFromList();
 });
+
+/**
+ * VIDEO BLOCKS:
+ */
 
 peerConnection.ontrack = function ({ streams: [stream] }) {
   const remoteVideo = document.getElementById("remote-video");
@@ -139,6 +136,7 @@ peerConnection.ontrack = function ({ streams: [stream] }) {
     };
   }
 };
+
 const options = {
   audio: true,
   video: {
@@ -151,7 +149,6 @@ const options = {
 navigator.mediaDevices
   .getUserMedia(options)
   .then((stream) => {
-
     const video = document.getElementById("local-video");
     video.setAttribute("playsinline", true);
     if (video) {
@@ -173,3 +170,34 @@ navigator.mediaDevices
   .catch((error) => {
     console.warn(error.message);
   });
+
+/**
+ * USER LIST:
+ */
+
+function updateUserList(socketIds) {
+  const activeUserContainer = document.getElementById("active-user-container");
+
+  socketIds.forEach((socketId) => {
+    const alreadyExistingUser = document.getElementById(socketId);
+    if (!alreadyExistingUser) {
+      const userContainerEl = createUserItemContainer(socketId);
+
+      activeUserContainer.appendChild(userContainerEl);
+    }
+  });
+}
+
+socket.on("update-user-list", ({ users }) => {
+  console.log("update-user<<<");
+  updateUserList(users);
+});
+
+socket.on("remove-user", ({ socketId }) => {
+  console.log("remove-user<<<");
+  const elToRemove = document.getElementById(socketId);
+
+  if (elToRemove) {
+    elToRemove.remove();
+  }
+});
